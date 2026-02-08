5cebf4d405aecde2ab78dc4a9bfe17227146633c
Sofia Serrano <syserrano@usf.edu>
2026-02-08 17:32:51 -0500

    Initial Client Array

---
+++ cc_3.js
@@ -0,0 +1,20 @@
+//array of customers with their name, email, and purchases
+let customers = [
+    {
+        name: "Ariana Grande",
+        email: "AG@music.com",
+        purchases: ["Mic", "Headphones", "Laptop"]
+    },
+    {
+        name: "Jungkook",
+        email: "JK@music.com",
+        purchases: ["Keyboard", "Mouse", "Monitor"]
+    },
+    {
+        name: "Kali Uchis",
+        email: "Kaliuchis88@music.com",
+        purchases: ["Guitar", "Amplifier", "Cables"]
+    }
+]
+
+console.log(customers[2].email)
